f58d786e60869d1bcf904aa585db3d0885040f64
Scuba-Chris <chrisceder@hotmail.com>
2019-09-30 17:39:03 -0700

    working on ejs -chris

--- server.js
+++ server.js
@@ -56,11 +56,11 @@ app.use('*', wildcard())
 
 
 
-function calendar(request, response){
-//find all things for specified month
+function calendar(request, response) {
+  //find all things for specified month
 
   let month = 'still need to create functionality to specify today\'s month';
-  let sql = `SELECT * FROM holidays WHERE month = ${month}`
+  let sql = `SELECT * FROM holidays WHERE month=${month}`
   pgClient.query(sql).then(oneMonthHolidays => {
 
     response.send(oneMonthHolidays);
@@ -68,30 +68,30 @@ function calendar(request, response){
 }
 
 
-function getOneDayHolidays(request, response){
+function getOneDayHolidays(request, response) {
   //find all things for specified day
 
   //line 82 may not work, depending on how the data is received
   const day_num = request.params.body.day;
-  let sql = `SELECT * FROM holidays WHERE day = ${day_num}`;
+  let sql = `SELECT * FROM holidays WHERE day=${day_num}`;
   pgClient.query(sql).then(oneDayHolidays => {
     let eventsForDay = oneDayHolidays.rows
 
-    response.render('/oneDay', {oneDayHolidays: eventsForDay})
+    response.render('/oneDay', { oneDayHolidays: eventsForDay })
   })
 }
 
-function addHoliday(request, response){
+function addHoliday(request, response) {
   // adds an event to the selected day
   //line 87 may not work, depending on how the data is received
   const day_num = request.params.body.day;
   const sqlInsert = 'INSERT INTO holidays (name, month, year, day, type, description) VALUES ($1, $2, $3, $4, $5);'
-  
+
   //insertArray will be incorrect, easily fixed when receiving form data (will not be formdata variable name)
   const insertArray = [formdata.name, formdata.month, formdata.year, formdata.day, formdata.type, formdata.description];
 
   pgClient.query(sqlInsert, insertArray).then(oneDayHolidays => {
-    response.render('/newHoliday', {oneDayHolidays :day_num})
+    response.render('/newHoliday', { oneDayHolidays: day_num })
   })
 }
 
