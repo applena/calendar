e8696ba77fe9edf706d540bc99b8b3a533844ca2
ENRIQUE RIVERA <KIK.RDL3@GMAIL.COM>
2019-10-02 20:00:18 -0700

    Commit to change machine

--- schema.sql
+++ schema.sql
@@ -6,8 +6,8 @@ DROP TABLE IF EXISTS holidays;
 CREATE TABLE holidays (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
-  month INTEGER,
   year INTEGER,
+  month INTEGER,
   day INTEGER,
   type VARCHAR(255),
   description VARCHAR(500)
--- server.js
+++ server.js
@@ -69,7 +69,7 @@ function hello(request, response){
 }
 
 function getCalendar(request, response) {
-  
+
   // 1. Read DB for Holidays.
   // 2. If Holidays in DB (and Holidays are for current month) return Holidays to client.
   // 3. Else read Calendarific API holidays data,
@@ -78,63 +78,126 @@ function getCalendar(request, response) {
   //   Read DB for Holidays,
   //   Return Holidays with id to client.
 
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth() + 1;
-  const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  let days = new Array(42).fill({});
-  days = days.map((el, index) => {
-    return {
-      date: `${weekdays[index % 7]}${index + 1}`,
-      day: index + 1,
-      holiday: ''
-    };
-  });
-  const formatHolidays = (holidays) => {
-    holidays.forEach(el => {
-      const dayNum = el.day;
-      days[dayNum - 1].holiday = el.name;
-    });
-  };
+  // const self = this;
 
   // Read DB for Holidays
-  db.readDBForOneHolidayEachDay()
-    .then(holidays => {
-      // If Holidays in DB return Holidays to client
+  let sql = 'SELECT DISTINCT ON (day) day, id, name, year, month, type, description FROM holidays ORDER BY day ASC;';
+  pgClient.query(sql)
+    .then(sqlRes => {
+      const holidays = sqlRes.rows;
+      // If Holidays in DB
       if (holidays[0]) {
-        // console.log('Hello world');
-        formatHolidays(holidays);
-
-        console.log('days', days);
-        // console.log('holidays', holidays);
-
-        // response.status(200);
-        response.render('index', { days: days });
-        // console.log(days)
+        // And Holidays are for current month
+        if (holidays[0].month === new Date().getMonth() + 1) {
+          // Return Holidays to client
+        } else {
+          // Else delete Holidays from DB
+          sql = 'DROP TABLE holidays;';
+          return pgClient.query(sql)
+            .then(() => new Promise(resolve => resolve(true)))
+            .catch(err => new Error(err).exit(response));
+        }
       } else {
-        // Else read Calendarific API holidays data
-        api.readAPI(year, month)
-          .then(holidays => {
-            // Create Holidays in DB
-            return Promise.all(holidays.map(holiday => {
-              return db.createDBByHoliday(holiday);
-            }));
-          })
-          .then(() => {
-            // Read DB for Holidays
-            db.readDBForOneHolidayEachDay()
-              .then(holidays => {
-                // Return Holidays with id to client
-                formatHolidays(holidays);
-
-                res.render('index', { days: days });
-              })
-              .catch(err => new Error(err).exit(res));
-          })
-          .catch(err => new Error(err).exit(res))
+        return new Promise(resolve => resolve(true));
       }
     })
-    .catch(err => new Error(err).exit(res));
+    .then(() => {
+      // Else read Calendarific API holidays data
+      const date = new Date();
+      const year = date.getFullYear();
+      const month = date.getMonth() + 1;
+      return api.readAPI(year, month);
+    })
+    .then(holidays => {
+      // Create Holidays in DB
+      const sql = 'INSERT INTO holidays (name, year, month, day, type, description) VALUES ($1, $2, $3, $4, $5, $6);';
+      return Promise.all(holidays.map(holiday => {
+        const qValues = [
+          holiday.name,
+          holiday.year,
+          holiday.month,
+          holiday.day,
+          holiday.type,
+          holiday.description
+        ];
+        return pgClient.query(sql, qValues);
+      }));
+    })
+    .then(() => {
+      // Read DB for Holidays
+      const sql = 'SELECT DISTINCT ON (day) day, id, name, year, month, type, description FROM holidays ORDER BY day ASC;';
+      return pgClient.query(sql);
+    })
+    .then(sqlRes => {
+      // Return Holidays with id to client
+      const holidays = sqlRes.rows;
+
+      // console.log(holidays);
+
+      const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+      let calendarDays = new Array(42).fill({});
+      calendarDays = calendarDays.map((calendarDay, index) => {
+        const holiday = holidays.find(holiday => {
+          return holiday.day === index + 1;
+        });
+
+        // console.log(holiday)
+
+        if (holiday) {
+          const date = new Date(`${holiday.year}-${holiday.month}-${holiday.day}`);
+          // const weekday = weekdays[date.getDay()];
+          return {
+            dayHeader: `${weekday} ${holiday.day}`,
+            holidayName: holiday.name,
+            year: holiday.year,
+            month: holiday.month,
+            day: holiday.day
+          };
+        }
+      });
+      console.log('calendarDays', calendarDays);
+      // console.log('length', calendarDays.length);
+
+
+      // const daysInMonth = new Date(holidays[0].year, holidays[0].month, 0).getDate();
+      // const firstDayInMonth = new Date(`${holidays[0].year}-${holidays[0].month}-${holidays[0].day}`);
+      // const renderHolidays = holidays.map((holiday, index, arr) => {
+      //   const weekday = weekdays[(firstDayInMonth.getDay() + index) % 7];
+
+      //   console.log(holiday.day);
+      //   console.log(arr[index - 1].day);
+
+      //   const holidayDayDifference = holiday.day - arr[index - 1].day;
+      //   if (holidayDayDifference === 0) {
+      //     return {
+      //       dayHeader: `${weekday} ${holiday.day}`,
+      //       holidayName: holiday.name,
+      //       year: holiday.year,
+      //       month: holiday.month,
+      //       day: holiday.day
+      //     };
+      //   } else {
+      //     for (let i = 0; i < holidayDayDifference; i++) {
+      //       return new Array(holidayDayDifference).fill({});
+      //     }
+      //   }
+      // });
+
+      // console.log(renderHolidays);
+
+      // for (let i = 0; i < firstDayInMonth.getDay(); i++) {
+      //   renderHolidays.unshift({});
+      // }
+
+      // for (let i = 0; i < daysInMonth; i++) {
+      //   renderHolidays.push({});
+      // }
+
+      response.render('index', {
+        calendarDays: calendarDays
+      });
+    })
+    .catch(err => new Error(err).exit(response));
 }
 
 // app.use('*', wildcard);
