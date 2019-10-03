0db86366bf89eb14f9f2f8727ddf63c0d615a5b2
Zerek <zerek.cover@gmail.com>
2019-10-03 16:06:04 -0700

    a little styling, and resetting schema file

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-DROP DATABASE IF EXISTS calendar;
-CREATE DATABASE calendar;
-\c calendar;
+-- DROP DATABASE IF EXISTS calendar;
+-- CREATE DATABASE calendar;
+-- \c calendar;
 
 DROP TABLE IF EXISTS holidays;
 CREATE TABLE holidays (
